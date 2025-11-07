63624da7b5fe5f3089f2591223450306234a0dce
xshiba <92438125+xshiba@users.noreply.github.com>
2025-11-07 23:16:41 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/FIXING/refs/heads/main/EE.lua"))()
 end)
-
+_G.LoadNew = true
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/16ddf2948fdb2a2791df613f2b969e7c.lua"))()
