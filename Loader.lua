30e1b0f08167525c5745ea4cf05a028130381b10
xshiba <92438125+xshiba@users.noreply.github.com>
2025-11-07 23:16:01 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/FIXING/refs/heads/main/EE.lua"))()
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/FIXING/refs/heads/main/EE.lua"))()
+end)
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/16ddf2948fdb2a2791df613f2b969e7c.lua"))()
