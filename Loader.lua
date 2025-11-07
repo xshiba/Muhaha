f95371a4bc85eccfda1f01c10fa697f467ea065b
xshiba <92438125+xshiba@users.noreply.github.com>
2025-11-07 23:26:12 +0700

    Adjust loading timing for external scripts

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 _G.LoadNew = true
 task.spawn(function()
-    wait(10)
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/16ddf2948fdb2a2791df613f2b969e7c.lua"))()
 end)
+wait(3)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/FIXING/refs/heads/main/EE.lua"))()
