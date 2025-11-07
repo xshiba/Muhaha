4b6b5b82a9a2e154491854b2a632ecda89ed331a
xshiba <92438125+xshiba@users.noreply.github.com>
2025-11-07 23:12:55 +0700

    Update and rename README.md to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/FIXING/refs/heads/main/EE.lua"))()
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/16ddf2948fdb2a2791df613f2b969e7c.lua"))()
